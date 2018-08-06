b5b5343b16ba688dba6229b8aaf5558781296f93
Cameron Judd <cameronjudd66@gmail.com>
2018-08-06 10:00:50 +1000

    Don't type Break

--- Week4-App1/Program.cs
+++ Week4-App1/Program.cs
@@ -10,8 +10,21 @@ namespace Week4_App1
     {
         static void Main(string[] args)
         {
+            int loopBreaker = 1;
+
             Console.WriteLine("Hello world!");
             Console.WriteLine("Workshop 4 edit");
+            Console.WriteLine("Cameron Judd was here.");
+
+            String breakString = "Break";
+            if (Console.ReadLine() == breakString)
+            {
+                for (int i = 0; i < loopBreaker; i++)
+                {
+                    loopBreaker++;
+                    Console.WriteLine(i);
+                }
+            }
 
             Console.ReadLine();
         }
